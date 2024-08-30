5968d2cf34dac6adebaf67284beae55873b71dcd
Konrad <xxxx>
2024-08-30 12:18:38 +0000

    Updating supplemental resources for v4 documentation deployment

--- v4/assets/Project.md.929wS-AP.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a4 as r}from"./chunks/framework.B94ikmwp.js";const f=JSON.parse('{"title":"UI5 Project","description":"","frontmatter":{},"headers":[],"relativePath":"Project.md","filePath":"Project.md"}'),a={name:"Project.md"},d=r("",26),i=[d];function c(s,n,l,p,u,h){return o(),t("div",null,i)}const y=e(a,[["render",c]]);export{f as __pageData,y as default};
---
+++ v4/site/assets/CLI.md._xUiuM-L.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a4 as s}from"./chunks/framework.Dq5zt9I_.js";const b=JSON.parse('{"title":"UI5 CLI","description":"","frontmatter":{},"headers":[],"relativePath":"CLI.md","filePath":"CLI.md"}'),i={name:"CLI.md"},o=s("",140),n=[o];function d(r,l,p,c,u,h){return a(),e("div",null,n)}const m=t(i,[["render",d]]);export{b as __pageData,m as default};
